b1fa20952f2d2ad3e008f4bde082b001c33828ed
dbruner23 <dbruner23@gmail.com>
2023-10-22 21:59:30 +1300

    basic map working

---
+++ src/Views/Helpers.ts
@@ -0,0 +1,40 @@
+import Map from '@arcgis/core/Map';
+import WebMap from '@arcgis/core/WebMap';
+import MapView from '@arcgis/core/views/MapView';
+import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
+import _isNil from 'lodash/isNil';
+
+export const buildMap = (successCallback: () => void, errorCallback: () => void) => {
+    debugger;
+    const basemapLayer = new VectorTileLayer({
+        url: "https://tiles.arcgis.com/tiles/XTtANUDT8Va4DLwI/arcgis/rest/services/nz_vector_basemap_v1/VectorTileServer"
+    })
+
+    const map = new WebMap(
+        {
+            // basemap: basemapLayer
+        }
+    )
+
+    const view = new MapView({
+        map: map,
+        container: "map-container",
+        center: [172.6306, -43.532],
+        zoom: 15
+    })
+
+    if (!_isNil(map) &&
+        !_isNil(view)) 
+    {
+        map.add(basemapLayer)
+        view.when(() => {
+            successCallback();
+            view.goTo({
+                center: [172.6306, -43.532],
+                zoom: 15,
+            })
+        })
+    } else {
+        errorCallback();
+    }
+}
---
+++ src/Views/MapComponent.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, useRef } from 'react'
+import Styles from './MapComponent.module.css'
+import { buildMap } from './Helpers';
+import { CircularProgress } from '@mui/material';
+
+const MapComponent = () => {
+    const mapRef = useRef<HTMLDivElement>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isError, setIsError] = useState<boolean>(false);
+
+    const setSuccessState = (): void => {
+        setIsLoading(false);
+        setIsError(false);
+    }
+
+    const setErrorState = (): void => {
+        setIsLoading(false);
+        setIsError(true);
+    }
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        if (mapRef.current) {
+            buildMap(setSuccessState, setErrorState);
+        }
+    }, []);
+
+    return (
+        <div>
+            {isLoading &&
+                <div className={Styles.mainContainer}>
+                    <CircularProgress data-test-id="loading-spinner" className={Styles.loading} />
+                    <div id="loading-text">
+                        <p>Loading map...</p>
+                    </div>
+                </div>
+            }
+            {isError &&
+                <div data-test-id="error-text" className={Styles.mainContainer}>
+                    <p>Something went wrong with loading the map. Please try again later.</p>
+                </div>
+            }
+            <div ref={mapRef} id="map-container" style={{ height: "100vh" }} />
+        </div>
+    )
+}
+
+export default MapComponent
