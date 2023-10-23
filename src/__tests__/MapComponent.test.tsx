c4b121bea838165e1770bf12b3063ccc98612a83
dbruner23 <dbruner23@gmail.com>
2023-10-24 12:52:59 +1300

    realised I forgot to add mock redux store in jest tests

--- src/__tests__/MapComponent.test.tsx
+++ src/__tests__/MapComponent.test.tsx
@@ -1,27 +1,57 @@
-import React from 'react';
-import MapComponent from '../Views/MapComponent';
-import { render, screen, waitFor, renderHook, act } from '@testing-library/react';
-import { buildMap } from '../Views/Helpers';
+import React from "react";
+import MapComponent from "../Views/MapComponent";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import SymbologyReducer, { colorSchemeState } from "../data/symbology/Reducer";
+import {
+  render,
+  screen,
+  waitFor,
+  renderHook,
+  act,
+  configure,
+} from "@testing-library/react";
 
-describe('MapComponent', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+const mockStore = configureStore([]);
 
-    it('renders MapComponent without crashing', () => {
-        render(<MapComponent />);
-    });
-    
-    it('displays a loading spinner when loading', async () => {
-        render(<MapComponent />);
-        expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
-    })
+const initialState = {
+  symbology: {
+    colorScheme: "YlGnBu",
+  },
+};
 
-    it('renders the map container after loading', async () => {
-        render(<MapComponent />);
-        await waitFor(() => {
-            expect(screen.getByTestId('map-container')).toBeInTheDocument();
-        });
+const store = mockStore(initialState);
+
+describe("MapComponent", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders MapComponent without crashing", () => {
+    render(
+      <Provider store={store}>
+        <MapComponent />
+      </Provider>
+    );
+  });
+
+  it("displays a loading spinner when loading", async () => {
+    render(
+      <Provider store={store}>
+        <MapComponent />
+      </Provider>
+    );
+    expect(screen.getByTestId("loading-spinner")).toBeInTheDocument();
+  });
+
+  it("renders the map container after loading", async () => {
+    render(
+      <Provider store={store}>
+        <MapComponent />
+      </Provider>
+    );
+    await waitFor(() => {
+      expect(screen.getByTestId("map-container")).toBeInTheDocument();
     });
-    
-})
+  });
+});
